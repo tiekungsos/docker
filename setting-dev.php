fe5ce1083a70bbbf5f229b7163b995998042f18d
thanapon.p <thanapon.p@aware.co.th>
2019-12-16 10:33:58 +0700

    commit

---
+++ setting-dev.php
@@ -0,0 +1,233 @@
+<?php
+$setting = array(
+	'debug' => 3,
+	// 'meeting' => array(
+	// 	'enable' => true,
+	// 	'start' => '2019-11-27 10:15:00',
+	// 	'end' => '2019-11-29 18:00:00',
+	// 	'outside_only' => false,
+    //     'available_bar' => false,
+	// 	'show_venue' => false,
+    //     'stand_name' => array(
+    //         'DEF' => '',
+    //     ),
+    //     'dedicated_venue' => 'DEF',
+    //     'dedicate_available' => array(
+    //         'start' => '2019-11-27',
+    //         'end' => '2019-11-29'
+    //     ),
+	// 	'multiple_agenda' => false,
+	// 	'block_first_slot' => false,
+	// 	'block_end_exhibitor' => array(9,10,11),
+	// 	'blockdate' => array(
+	// 		// '2019-12-04' => array(11),
+	// 		// '2019-12-04' => array(11)
+	// 	)
+    // ),
+    // 'login' => array(
+    //     'max_attemp' => 3,
+    // ),
+    // 'console' => array(
+    //     'url' => 'https://arabhealth.livehealthconnect.com/app/',
+    // ),
+    // 'cms' => array(
+    //     'url' => 'https://arabhealth.livehealthconnect.com/app/',
+    //     'base_url' => 'https://arabhealth.livehealthconnect.com/app/',
+    // ),
+    // 'smtp' => array(
+    //     'from' => array('admin@livehealthconnect.com' => 'Arab Health 2020'),
+	// 	'host' => 'mailhog',
+	// 	'port' => 1025,
+	// 	'timeout' => 30
+    // ),
+    // 'email' => array(
+	// 	'admin' => 'admin@livehealthconnect.com',
+    //     'event' => 'Arab Health 2020',
+	// 	'verify' => array(
+	// 		'subject' => 'You have successfully registered - Please verify your e-mail address',
+	// 		'template' => 'verify_email',
+    //         'cc' => array('admin@livehealthconnect.com')
+	// 	),
+	// 	'request_category' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'subject' => 'Request new subcategory',
+	// 		'template' => 'request_category'
+	// 	),
+	// 	'request_tradefair' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'subject' => 'Request new trade fair',
+	// 		'template' => 'request_tradefair'
+	// 	),
+	// 	'verify_new_email' => array(
+	// 		'subject' => 'Please click to confirm your e-mail address',
+	// 		'template' => 'verify_new_email',
+	// 		'cc' => array('admin@livehealthconnect.com')
+	// 	),
+	// 	'suspend_member' => array(
+	// 		'subject' => 'Account Suspension Notification',
+	// 		'template' => 'suspend_member'
+	// 	),
+	// 	'unsuspend_member' => array(
+	// 		'subject' => 'Account Suspension Notification',
+	// 		'template' => 'unsuspend_member'
+	// 	),
+	// 	'billing_expire' => array(
+	// 		'subject' => 'Billing Expire',
+	// 		'template' => 'billing_expire'
+	// 	),
+	// 	'suggest_member' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'subject' => 'New member suggestion',
+	// 		'template' => 'company_suggest_member'
+	// 	),
+    //     'weekly_status'=> array(
+	// 		'subject' => 'Your weekly meeting and matching summary',
+	// 		'template' => 'weekly_summary'
+	// 	),
+    // 	'change_company_name' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'subject' => '[FI] Company Name Change Notification.',
+    //         'template' => 'change_company_name',
+    //         'layout' => 'blank'
+	// 	),
+	// 	'change_company_email' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'subject' => '[FI] User E-Mail Address Change Notification.',
+    //         'template' => 'change_company_email',
+    //         'layout' => 'blank'
+	// 	),
+	// 	'payment_request' => array(
+	// 		'template' => 'payment_request'
+	// 	),
+	// 	'invite_member' => array(
+	// 		'subject' => 'Invitation to join Live Pharma Connect – Connecting the medical world',
+	// 		'template' => 'invite_member'
+	// 	),
+	// 	'notify_payment' => array(
+	// 		'template' => 'notify_payment'
+	// 	),
+	// 	'notify_trial_expired' => array(
+	// 		'template' => 'notify_trial_expired'
+	// 	),
+	// 	'notify_trial_expired_3w' => array(
+	// 		'template' => 'notify_trial_expired_3w'
+	// 	),
+	// 	'notify_trial_expired_2w' => array(
+	// 		'template' => 'notify_trial_expired_2w'
+	// 	),
+	// 	'notify_trial_expired_1w' => array(
+	// 		'template' => 'notify_trial_expired_1w'
+	// 	),
+	// 	'notify_trial_expired_3d' => array(
+	// 		'template' => 'notify_trial_expired_3d'
+	// 	),
+	// 	'notify_trial_update' => array(
+	// 		'template' => 'notify_trial_update_3m'
+	// 	),
+	// 	'send_receipt_file' => array(
+	// 		'template' => 'send_receipt_file',
+	// 		'subject' => 'Your payment receipt'
+	// 	),
+	// 	'front_end_contact' => array(
+	// 		'to' => 'admin@livehealthconnect.com',
+	// 		'from' => 'admin@livehealthconnect.com',
+	// 		'subject' => 'Public Site Contact form',
+	// 		'template' => 'contact'
+	// 	),
+	// 	'feedback' => array(
+	// 		'nonmember' => array(
+	// 			'to' => 'admin@livehealthconnect.com',
+	// 			'from' => 'admin@livehealthconnect.com',
+	// 		)
+	// 	),
+	// 	'inviteexhibitor' => array(
+	// 		'template' => 'invite_exhibitor'
+	// 	),
+	// 	'invitevisitor' => array(
+	// 		'template' => 'invite_visitor'
+	// 	)
+	// ),
+    // 'email_request' => array(
+	// 	'sponsorship' => array(
+	// 		'to' => array('fisalesoperations@ubm.com','Carina.Weymans@informa.com'),
+	// 		'cc' => array('support@matchingredients.com'),
+	// 		'subject' => 'Sponsorship Opportunities Request',
+	// 		'template' => 'sponsorship',
+	// 	),
+	// 	'visitor2visitor' => array(
+	// 		'to' => array('fisalesoperations@ubm.com','Carina.Weymans@informa.com'),
+	// 		'cc' => array('support@matchingredients.com'),
+	// 		'subject' => 'Visitor-Visitor Matching Service Request',
+	// 		'template' => 'visitor2visitor',
+	// 	),
+	// 	'hostedpartner' => array(
+	// 		'to' => array('fisalesoperations@ubm.com','Carina.Weymans@informa.com'),
+	// 		'cc' => array('support@matchingredients.com'),
+	// 		'subject' => 'Hosted Partner Programme Request',
+	// 		'template' => 'hosted_partner_programme',
+	// 	),
+	// 	'private_meeting_room' => array(
+	// 		'to' => array('fisalesoperations@ubm.com','Carina.Weymans@informa.com'),
+	// 		'cc' => array('support@matchingredients.com'), 
+	// 		'subject' => 'Exclusive FI Meeting Area Request',
+	// 		'template' => 'private_meeting_rooms',
+	// 	)
+
+	// ),
+    // 'service_n200' => array(
+	// 	'active' => false,
+	// 	'uri' => 'https://api2.n200.com',
+	// 	'key' => "160f2b57-9932-4ab3-a923-7d07e3562795",
+	// 	'event_code' => '0t080fsico6vc',
+	// 	'event' => '/events',
+	// 	'question' => '/questions',
+	// 	'visitor' => '/visitors',
+	// 	'partner' => '/partners',
+	// 	'visitor_contact' => '/contacts',
+	// 	'visitor_profile' => '/visitor_profiles'
+	// ),
+	// 'event' => array(
+    //     'name' => 'Arab Health 2020',
+    //     'cphi_name' => 'FI 2019',
+	// 	'short_name' => 'Live Health Connect',
+	// 	'email_name' => 'business matchmaking',
+    //     'support_email' => 'admin@livehealthconnect.com',
+    //     'provider' => 'CPhI',
+    //     'show_partner' => false,
+    //     'lpc_name' => 'Business Matchmaking',
+    //     'has_venue' => true,
+	// 	'allow_update_admin_create' => true,
+	// 	'close_date' => '2020-12-06'
+    // ),
+	// 'web' => array(
+	// 	'canlogin' => true,
+	// 	'canlogindate' => '2020-10-21',
+    // ),
+	// 'shell' => array(
+	// 	'limitRunning' => 1445,
+	// 	'limitCron' => 175,
+	// 	'MatchingGenerate' => array(
+	// 	'generateData' => array('enable' => true),
+	// 	'sendEmail' => array('enable' => true, 'day' => array(3)),
+    //     'sendEmailWeekly' => array('enable' => true)
+	// 	),
+	// 	'NotifyExpired' => array(
+	// 		'enable' => true,
+	// 		'email_send_advance_day' => 14
+	// 	),
+	// 	'NotifyTrialExpired' => array(
+	// 		'enable' => true,
+	// 		'email_send_advance_day_3w' => 21,
+	// 		'email_send_advance_day_2w' => 14,
+	// 		'email_send_advance_day_1w' => 7,
+	// 		'email_send_advance_day_3d' => -3,
+	// 		'email_send_advance_month_3m' => -3,
+	// 		'email_send_advance_month_6m' => -6,
+	// 		'email_send_advance_month_9m' => -9,
+	// 		'email_send_advance_month_12m' => -12,
+	// 	),
+	// 	'UpdateExpireStatus' => array(
+	// 		'enable' => true
+	// 	),
+	// ),
+);
